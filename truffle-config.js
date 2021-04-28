1070418ff00d4442a5b7fec8cb770099ab95c6ca
NetanelAmar <40565540+Coinsandicos@users.noreply.github.com>
2021-04-28 10:43:13 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,21 @@
 module.exports = {
   networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      gas:550000,
-      gasPrice:25000000000,
-      network_id: '*' // Match any network id
-    }
+    // development: {
+    //   host: 'localhost',
+    //   port: 8545,
+    //   gas:550000,
+    //   gasPrice:25000000000,
+    //   network_id: '*' // Match any network id
+    // },
+    ropsten: {
+      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApiKey}`),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,
+      networkCheckTimeout: 99999, // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
   solc: {
     // Turns on the Solidity optimizer. For development the optimizer's
@@ -17,7 +26,7 @@ module.exports = {
     // https://github.com/trufflesuite/truffle/releases/tag/v4.0.0
     optimizer: {
       enabled: true,
-      runs: 200
-    }
-  }
-}
+      runs: 200,
+    },
+  },
+};
