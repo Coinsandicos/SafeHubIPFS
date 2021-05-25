4142583dd1e44b204a2a93c8d4ef77a4991f28a1
NetanelAmar <40565540+Coinsandicos@users.noreply.github.com>
2021-05-25 12:45:04 +0300

    Update auth.js

--- client/pages/auth.js
+++ client/pages/auth.js
@@ -1,23 +1,23 @@
 import React from "react";
 import Link from "next/link";
 
-const passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
+// const passport = require('passport')
+//   , LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function(err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
 
 export default () => (
   <div>
